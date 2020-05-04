549dc1cc175755f230bb0ccd460091a258dd10c5
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-05-04 15:13:36 -0400

    completed login validation

--- scripts/login.js
+++ scripts/login.js
@@ -2,20 +2,24 @@ console.log('hello log in world')
 
 const loginInput = document.querySelector('#username-login-input')
 const loginBtn = document.querySelector('#login-btn')
-let hasAccount;
+let hasAccount = false
 
 loginBtn.addEventListener('click', event => {
+  hasAccount = false
+
   fetch(testingEndpoint)
   .then(resp => resp.json())
   .then(users => {
     users.forEach(validateUser)
 
     hasAccount ? alert(`Welcome back ${loginInput.value}`) : alert('you don\'t have an account')
+
+    loginInput.value = ''
   })
 })
 
 function validateUser(userObj) {
   if (loginInput.value === userObj.name) {
     hasAccount = true
-  }
+  } 
 }
