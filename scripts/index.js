a81501ea1a200fb523fba859c1cc5a9e099b702d
CBreakr <github email address>
2020-05-05 10:31:23 -0400

    moved into main container

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,28 @@
-console.log('hello world')
+
+function showInitialView(){
+    // hide sidebar content
+    // hide question header
+    // show the login and registration
+}
+
+function showMainQuestionView(){
+    // hide login and registration
+    // hide question form
+    // hide question detail
+    // show sidebar content
+    // show question header
+    // show question list
+}
+
+function showQuestionFormView(){
+    // hide question list
+    // hide question detail
+    // show question form
+}
+
+function showQuestionDetailView(){
+    // hide question list
+    // hide question form
+    // show question detail
+}
 
