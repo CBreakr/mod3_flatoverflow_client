70745c6bfb1284d360d53ed654591adee2672e6e
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-05-07 13:42:47 -0400

    add logic to create follows

---
+++ scripts/user_follow.js
@@ -0,0 +1,9 @@
+console.log('user_follow.js loaded')
+console.log(questionUL)
+
+// function someMethod(div) {
+//   let questionBasicDiv = div
+//   console.log(div)
+//   let user_id = div.children[0].dataset.user_id
+//   console.log(user_id)
+// }
