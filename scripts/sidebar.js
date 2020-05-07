48227534f367c037e6a17bcc0885ef91e7740056
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-05-06 22:38:36 -0400

    change html and render trending tags logic

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -2,15 +2,7 @@ console.log('sidebar.js loaded')
 
 const trendingTagsEndpoint = 'http://localhost:3000/tags/trending'
 
-let sidebarDiv = document.getElementById('sidebar')
-
-let div = document.createElement('div')
-div.id = 'trending-tags'
-div.innerHTML = `
-    <ul>
-    </ul>
-`
-sidebarDiv.replaceChild(div, sidebarDiv.children[0])
+let trendingList = document.getElementById('trending_tags_list')
 
 function getPopularTags() {
   fetch(trendingTagsEndpoint)
@@ -20,11 +12,11 @@ function getPopularTags() {
 
 function renderTrendingTags(tags) {
   tags.forEach(tag => {
-    let div = document.getElementById('trending-tags')
     let li = document.createElement('li')
+    li.className = "question_tag tag_link"
     li.dataset.id = tag.id
     li.innerText = tag.text
-    div.append(li)
+    trendingList.append(li)
   })
 }
 
